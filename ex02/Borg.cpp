8a90a4d8863ae66b03d452d257df9fb6f2396565
petit_x <petit_x@epitech.eu>
2016-01-12 15:49:14 +0100

    02

---
+++ ex02/Borg.cpp
@@ -0,0 +1,47 @@
+/*
+** Borg.cpp for Borg/home/petit_x/Epitech/rendu/piscine_cpp/piscine_cpp_d07m/ex01/Borg.cpp
+** 
+** Made by  petit_x - Marlon Petit
+** Login   < petit_x@epitech.net >
+** 
+** Started on  Tue Jan 12 14:27:11 2016 petit_x - Marlon Petit
+** Last update Tue Jan 12 15:45:41 2016 marlon petit
+*/
+
+#include <iostream>
+#include <string>
+
+#include "Warpsystem.hh"
+#include "Federation.hh"
+#include "Borg.hh"
+#include "Destination.hh"
+
+Borg::Ship::Ship()
+{
+  this->_side = 300;
+  this->_maxWarp = 9;
+  this->_home = UNICOMPLEX;
+  this->_location = UNICOMPLEX;
+  
+  std::cout << "We are the Borgs. Lower your shields and surrender yourselves unconditionally." << std::endl << "Your biological characteristics and technologies will be assimilated." << std::endl << "Resistance is futile." << std::endl;
+}
+
+void	Borg::Ship::setupCore(WarpSystem::Core *core)
+{
+  this->_core = core;
+}
+
+void	Borg::Ship::checkCore()
+{
+  if (this->_core->checkReactor()->isStable() == true)
+    std::cout << "Everything is in order." << std::endl;
+  else
+    std::cout << "Critical failure imminent." << std::endl;
+}
+
+bool    Borg::Ship::move(int warp, Destination d)
+{
+  if (warp <= this->_maxWarp && d != this->_location && this->_core->checkReactor()->isStable() == true)
+    return (true);
+  return (false);
+}
---
+++ ex02/Federation.hh
@@ -0,0 +1,79 @@
+/*
+** Federation.hh for piscine_cpp_d07m
+** 
+** Made by  petit_x - Marlon Petit
+** Login   < petit_x@epitech.net >
+** 
+** Started on  Tue Jan 12 11:40:10 2016 petit_x - Marlon Petit
+** Last update Tue Jan 12 15:38:54 2016 marlon petit
+*/
+
+#ifndef __FEDERATION_HH__
+# define __FEDERATION_HH__
+
+#include <iostream>
+#include <string>
+
+#include "Warpsystem.hh"
+#include "Borg.hh"
+#include "Destination.hh"
+
+namespace		Federation
+{
+  class			Organisation
+  {};
+  namespace       Starfleet
+  {
+    class               Captain
+    {
+    public:
+      Captain(std::string);
+      std::string       getName();
+      int               getAge();
+      void              setAge(int);
+      std::string       _name;
+      int               _age;
+    };
+    class               Ensign
+    {
+    public:
+      explicit Ensign(std::string);
+      std::string       _name;
+    };
+    class         Ship 
+    {
+    public:
+      Ship(int length, int width, std::string name, short maxWarp);
+      void	setupCore(WarpSystem::Core *);
+      void	checkCore();
+      void	promote(Federation::Starfleet::Captain *);
+      bool                move(int warp = 0, Destination d = EARTH);
+      int         _length;                               
+      int         _width;
+      std::string    _name;
+      short         _maxWarp;
+      Destination	_location;
+      Destination	_home;
+      Federation::Starfleet::Captain	*_captain;
+      WarpSystem::Core	*_core;
+    };
+  };
+
+  class         Ship                                                                                    
+  {                                                                                                     
+  public:                                                                                               
+    Ship(int length, int width, std::string name);                                       
+    void      setupCore(WarpSystem::Core *);
+    void      checkCore();
+    bool                move(int warp = 0, Destination d = VULCAN);
+    int         _length;                                                                                
+    int         _width;       
+    Destination       _location;                                                                        
+    Destination       _home;
+    std::string    _name;                                                                               
+    short         _maxWarp;                                                                             
+    WarpSystem::Core      *_core;                  
+  };
+};
+
+#endif /* !__FEDERATION_HH__ */
