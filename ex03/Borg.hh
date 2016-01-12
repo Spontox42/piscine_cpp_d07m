5105a8d3bca79dfd8deb22730b4ea50dbb4c4fb0
petit_x <petit_x@epitech.eu>
2016-01-12 15:53:47 +0100

    03_init

---
+++ ex03/Borg.hh
@@ -0,0 +1,38 @@
+/*
+** Borg.hh for Borg/home/petit_x/Epitech/rendu/piscine_cpp/piscine_cpp_d07m/ex01/Borg.hh
+** 
+** Made by  petit_x - Marlon Petit
+** Login   < petit_x@epitech.net >
+** 
+** Started on  Tue Jan 12 14:25:04 2016 petit_x - Marlon Petit
+** Last update Tue Jan 12 15:42:11 2016 marlon petit
+*/
+
+#ifndef BORG_HH_
+# define BORG_HH_
+
+#include <iostream>
+#include <string>
+
+#include "Warpsystem.hh"
+#include "Federation.hh"
+#include "Destination.hh"
+
+namespace		Borg
+{
+  class			Ship
+  {
+  public:
+    Ship();
+    void		setupCore(WarpSystem::Core *);
+    void		checkCore();
+    bool		move(int warp = 0, Destination d = UNICOMPLEX);
+    int			_side;
+    short		_maxWarp;
+    Destination		_home;
+    Destination		_location;
+    WarpSystem::Core	*_core;
+  };
+};
+
+#endif /* !BORG_HH_ */
